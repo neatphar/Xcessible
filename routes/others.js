4fe811e46ab123ed7eedda44052dd30edd9e00ba
neatphar <neatphar@gmail.com>
2021-10-17 19:24:42 +0200

    fixes

--- routes/others.js
+++ routes/others.js
@@ -54,12 +54,14 @@ exports.login = function(req, res){
 exports.logout = function(req, res) {
 	req.session.username = ''
 	req.session.password = ''
+	req.session.name = ""
 	req.session.logged = false;
 	res.redirect('/');
 };
 
 
 exports.search_view = function(req, res){
+	res.locals.name = req.session.name;
 	res.locals.username = req.session.username;
 	res.locals.password = req.session.password;
 	var locations = [];
@@ -86,6 +88,7 @@ exports.search_view = function(req, res){
 };
 
 exports.search = function(req, res){
+	res.locals.name = req.session.name;
 	res.locals.username = req.session.username;
 	res.locals.password = req.session.password;
 
@@ -94,11 +97,11 @@ exports.search = function(req, res){
 	};
 	var locations = req.body.location.split("|").filter(n => n);
 	if (locations.length > 0){
-		keys["Location"] = {"$in" : locations};
+		keys["Location"] = {"$in": locations};
 	}
 	var disabilities = req.body.disabilities.split("|").filter(n => n);
 	if (disabilities.length > 0){
-		keys["RejectedDisabilities"] = {"$nin" : disabilities};
+		keys["RejectedDisabilities"] = {"$nin": disabilities};
 	}
 	Job.find(keys, function(err, jobs){  	
 		res.render('search_results', { 
@@ -130,7 +133,7 @@ exports.profile = function(req, res){
 	res.locals.name = req.session.name;
 	res.locals.username = req.session.username;
 	res.locals.password = req.session.password;
-	Job.find({Author:req.session.username}, function(err, jobs){  	
+	Job.find({Author: {"$in": [req.session.username, req.session.name]}}, function(err, jobs){  	
 		res.render('profile', { 
 			jobs: jobs
 		});
@@ -142,6 +145,7 @@ exports.update_a_job_view = function(req, res){
 		res.redirect('/');
 		return;
 	} 	
+	res.locals.name = req.session.name;
 	res.locals.username = req.session.username;
 	res.locals.password = req.session.password;
 	Job.find({_id: req.params.jobId}, function(err, jobs){
@@ -161,6 +165,7 @@ exports.update_a_job = function(req, res){
 		res.redirect('/');
 		return;
 	} 	
+	res.locals.name = req.session.name;
 	res.locals.username = req.session.username;
 	res.locals.password = req.session.password;
 	Job.find({_id: req.params.jobId}, function(err, jobs){
@@ -176,7 +181,7 @@ exports.update_a_job = function(req, res){
 			}else{
 				Job.update({_id: req.params.jobId}, 
 					{
-						Author: req.session.username,
+						Author: req.session.name,
 						IconLink: req.body.IconLink,
 						Employer: req.body.Employer,
 						Title: req.body.Title,
@@ -202,6 +207,7 @@ exports.update_a_job = function(req, res){
 
 
 exports.post_a_job_view = function(req, res){
+	res.locals.name = req.session.name;
 	res.locals.username = req.session.username;
 	res.locals.password = req.session.password;
 	res.render('post_a_job');
@@ -228,110 +234,3 @@ exports.post_a_job = function(req, res){
 	});
 };
   
-
-
-/*
-exports.create = function(req, res){
-	if(!req.session.logged) {
-		res.redirect('/');
-		return;
-	} else {
-		res.locals.username = req.session.username;
-  		res.locals.password = req.session.password;
-		res.render('blog/create', { title: 'Node Express Job'});
-	}
-}
-
-exports.create_post = function(req, res){
-	if(!req.session.logged) {
-		res.redirect('/');
-		return;
-	} 	
-	new Job({
-		Author: req.session.username,
-		Title: req.body.title,
-		Content: req.body.content,
-		CreatedTime: Date.now()
-	}).save(function(err){
-		if(err) {
-			console.log('fail to add post');
-		} else {
-			console.log('add post success!');
-			res.redirect('/');
-			return;
-		}
-	})
-};
-
-exports.profile = function(req, res){
-	if(!req.session.logged) {
-		res.redirect('/');
-		return;
-	} 	
-	Job.find({Author: req.session.username}, function(err, blogs){
-		if(blogs.length == 0) {
-			console.log('you dont have any post');
-			res.redirect('/');
-			return;
-		} else {
-			res.locals.username = req.session.username;
-  			res.locals.password = req.session.password;
-			res.render('blog/profile', {title: 'Node Express Job', blogs: blogs});
-		}
-	})
-};
-
-exports.update = function(req, res){
-	if(!req.session.logged) {
-		res.redirect('/');
-		return;
-	} 	
-	res.locals.username = req.session.username;
-	res.locals.password = req.session.password;
-	Job.find({_id: req.params.postId}, function(err, blog){
-		if(err) {
-			console.log('fail to find update');
-		} else {
-			res.render('blog/update', {title: 'Node Express Job', blog: blog, postId: req.params.postId});
-			console.log('find update post');
-		}
-	});
-};
-
-exports.update_post = function(req, res){
-	if(!req.session.logged) {
-		res.redirect('/');
-		return;
-	} 	
-	Job.update({_id: req.params.postId}, {Title: req.body.title, Content: req.body.content}, function(err){
-		if(err) {
-			console.log('update fail');
-		} else {
-			console.log('update.success');
-			res.locals.username = req.session.username;
-  			res.locals.password = req.session.password;			
-			res.redirect('/');
-			return; 
-		}
-	});
-};
-
-exports.delete_post = function(req, res){
-	if(!req.session.logged) {
-		res.redirect('/');
-		return;
-	} 	
-	Job.remove({_id: req.params.postId}, function(err){
-		if(err) {
-			console.log('remove fail');
-		} else {
-			console.log('remove success!');
-			res.locals.username = req.session.username;
-  			res.locals.password = req.session.password;			
-			res.redirect('/');
-			return; 
-		}
-	});
-}
-
-*/
